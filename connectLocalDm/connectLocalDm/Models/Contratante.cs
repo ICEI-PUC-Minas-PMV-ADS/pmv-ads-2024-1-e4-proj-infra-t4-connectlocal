6ba70ecf9c9d550bb2bdb0dbe007ded649212910
Daniel Mendes <dmendesrbh@gmail.com>
2024-03-13 08:09:14 -0300

    Initial commit

---
+++ connectLocalDm/connectLocalDm/Models/Contratante.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace connectLocalDm.Models
+{
+    [Table("Contratantes")]
+    public class Contratante
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string NomeContratante { get; set; }
+        [Required]
+        public string EndContratante { get; set; }
+        [Required]
+        public string ContatoContratante { get; set; }
+        [Required]
+        public string EmailContratante { get; set; }
+        [Required]
+        public string CPF { get; set; }
+
+    }
+}
---
+++ connectLocalDm/connectLocalDm/Models/Prestador.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace connectLocalDm.Models
+{
+    [Table("Prestadores")]
+    public class Prestador
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string NomePrestador { get; set; }
+        [Required]
+        public TipoServico Tipo { get; set; }
+        [Required]
+        public string InfoServico { get; set; }
+        [Required]
+        public string EnderecoServico { get; set; }
+        [Required]
+        public string ContatoServico { get; set; }
+        [Required]
+        public string EmailServico { get; set; }
+        [Required]
+        public string CNPJ { get; set; }
+
+    }
+
+    public enum TipoServico
+    {
+        Borracheiro,
+        ConsertoEletrodomesticos,
+        CuidadorIdosos,
+        Eletricista,
+        Encanador,
+        Pintor,
+        ProfIngles,
+        ProfMatematica,
+    }
+}
